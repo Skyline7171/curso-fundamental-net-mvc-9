d5a8c97d7505e6d9cb26da80f7daf6026464f9a2
Sky <xpolargeist007x@gmail.com>
2026-06-19 00:04:10 -0600

    Curso finalizado

---
+++ CrudNet7MVC/Models/Contacto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudNet7MVC.Models
+{
+    public class Contacto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El número de teléfono es obligatorio")]
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "El número de célular es obligatorio")]
+        public string Celular { get; set; }
+        [Required(ErrorMessage = "El correo electrónico es obligatorio")]
+        public string Email { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
